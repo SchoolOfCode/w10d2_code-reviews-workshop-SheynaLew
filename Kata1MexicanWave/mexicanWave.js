d5a51a9d2a9bad4f0431f04587edd13b5bdffb49
Jenny <jenny.m.lewis@hotmail.co.uk>
2022-01-25 11:55:02 +0000

    Plan for Kata1 written

--- Kata1MexicanWave/mexicanWave.js
+++ Kata1MexicanWave/mexicanWave.js
@@ -1,5 +1,16 @@
 function wave(str) {
-  // Code here
+  
+
+  //go through the array, converting one letter at a time to a capital and back again
+    //use a for loop on the string 
+    //create a second variable to equal the first string but with a letter capitalised. 
+      //str.charAt(i).toUpperCase + str.Slice(i+1)  
+        //this capitalises the letter and then adds it to the word
+    //push the new string into an array
+    //if there is whitespace, the for loop needs to skip it
+      //if i=" " i++ else do the above
+      //Maybe need to use str.split to seperate each word?
+
 }
 
 export default wave;
