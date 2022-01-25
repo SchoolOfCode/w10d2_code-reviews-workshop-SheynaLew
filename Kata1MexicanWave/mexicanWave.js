e77002bccd8b4c854488791f87d2583426d5f495
Jenny <jenny.m.lewis@hotmail.co.uk>
2022-01-25 12:00:39 +0000

    for loop and part of capitalisation written

--- Kata1MexicanWave/mexicanWave.js
+++ Kata1MexicanWave/mexicanWave.js
@@ -1,5 +1,8 @@
 function wave(str) {
-  
+  for (i=0, i<str.length, i++) {
+    let newStr = str.charAt(i.toUppderCase);
+    console.log(newStr);
+  }
 
   //go through the array, converting one letter at a time to a capital and back again
     //use a for loop on the string 
@@ -9,8 +12,8 @@ function wave(str) {
     //push the new string into an array
     //if there is whitespace, the for loop needs to skip it
       //if i=" " i++ else do the above
-      //Maybe need to use str.split to seperate each word?
+      //Maybe need to use str.split to seperate each word? and then .join to join the array with " " to maintain the whitespace?
 
 }
-
+wave("hello world")
 export default wave;
